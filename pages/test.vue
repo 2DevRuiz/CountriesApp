8d0cac036158911d301c86aaf3b60b6d9b7c8f80
Darwing Ruiz <darjruiz11@gmail.com>
2023-08-07 15:31:36 -0600

    templates for us

--- pages/test.vue
+++ pages/test.vue
@@ -96,4 +96,11 @@
             </div>
         </div>
     </div>
+    <!-- <button onclick="buttonHandler()" title="Contact Sale"
+        class="fixed z-90 bottom-10 right-8 bg-blue-600 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-100">&#9993;</button> -->
+    <div class="fixed bottom-4 right-4">
+        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg">
+            Feedback
+        </button>
+    </div>
 </template>
